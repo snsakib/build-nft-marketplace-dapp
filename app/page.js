f3b67086eeab68618cd2cffb15fec3f01f5b3ce3
snsakib <hello@snsakib.com>
2023-12-03 02:06:05 +0600

    update global css

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-center mt-[200px] text-4xl">Welcome to the course!</h1>
+    <h1 className="text-center mt-[200px] text-4xl text-black">Welcome to the course!</h1>
   );
 }
