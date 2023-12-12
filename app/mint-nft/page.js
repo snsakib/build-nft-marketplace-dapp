68b869d38a1e0d50350a6a556dbb862cc0ef3dff
snsakib <hello@snsakib.com>
2023-12-12 18:28:07 +0600

    Add MintNFT component for creating NFTs

---
+++ app/mint-nft/page.js
@@ -0,0 +1,73 @@
+"use client";
+import { useState } from "react";
+
+export default function MintNFT() {
+  const [nftData, setNftData] = useState({
+    name: "",
+    price: "",
+  });
+  const [file, setFile] = useState(null);
+  const [msg, setMsg] = useState("");
+
+  return (
+    <div className="flex flex-col items-center py-10">
+      <h1 className="text-3xl font-bold text-sky-900 mb-5">Create NFT</h1>
+      <form className="border-2 border-sky-900 rounded-md flex flex-col justify-around items-center min-h-max p-10">
+        <div className="flex flex-col justify-around items-start mb-5 min-w-full">
+          <label htmlFor="name" className="text-xl font-bold text-sky-900">
+            NFT Name <span className="text-red-700">*</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="min-w-full mt-3 rounded min-h-[35px] text-black p-2 border-2 border-sky-900"
+            value={nftData.name}
+            onChange={(e) => setNftData({ ...nftData, name: e.target.value })}
+            required
+          />
+        </div>
+        <div className="flex flex-col justify-around items-start my-5 min-w-full">
+          <label htmlFor="image" className="text-xl font-bold text-sky-900">
+            Upload Image <span className="text-red-700">*</span>
+          </label>
+          <input
+            type="file"
+            name="image"
+            id="image"
+            className="min-w-full mt-3 rounded min-h-[35px] text-black p-2 file:bg-white file:text-sky-900 file:font-semibold file:rounded file:mr-2 file:py-1 file:px-2 file:border-0 file:border-rounded"
+            accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
+            required
+          />
+        </div>
+        <div className="flex flex-col justify-around items-start my-5 min-w-full">
+          <label htmlFor="price" className="text-xl font-bold text-sky-900">
+            Price (in ETH) <span className="text-red-700">*</span>
+          </label>
+          <input
+            type="number"
+            name="price"
+            id="price"
+            className="min-w-full mt-3 rounded min-h-[35px] text-black p-2 border-2 border-sky-900"
+            value={nftData.price}
+            onChange={(e) => setNftData({ ...nftData, price: e.target.value })}
+            required
+          />
+        </div>
+        <div>
+          <p>{msg}</p>
+        </div>
+        <div className="flex flex-col justify-around items-start mt-5 min-w-full">
+          <button
+            id="submit"
+            className="rounded font-bold text-xl min-w-full min-h-[35px] bg-blue-500"
+            type="submit"
+          >
+            Create NFT
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
