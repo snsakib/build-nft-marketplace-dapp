9d49a9091eddb5203e089afe73cf9009fdb2e90c
snsakib <hello@snsakib.com>
2023-12-22 09:12:32 +0600

    Refactor pinning logic for NFT file and metadata

--- app/api/mint-nft/route.js
+++ app/api/mint-nft/route.js
@@ -1,60 +1,84 @@
+// Import the NextResponse object from the "next/server" module
 import { NextResponse } from "next/server";
 
-export async function POST(request) {
-  let formData = await request.formData();
-  const NFT_NAME = formData.get('name');
-  const NFT_PRICE = formData.get('price');
-  const NFT_FILE = formData.get('file');
-  let nftImgCID = '';
-  let nftMetadataCID = '';
+// Define Pinata API endpoints and headers
+const PINATA_FILE_URL = "https://api.pinata.cloud/pinning/pinFileToIPFS";
+const PINATA_METADATA_URL = "https://api.pinata.cloud/pinning/pinJSONToIPFS";
 
-  const PIN_FILE_URL = "https://api.pinata.cloud/pinning/pinFileToIPFS";
-  const PIN_METADATA_URL = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+let pinataHeaders = {
+  Authorization: `Bearer ${process.env.PINATA_JWT_TOKEN}`,
+};
 
-  let fileData = new FormData();
-  fileData.append("file", NFT_FILE);
-
-  let fileMetadata = JSON.stringify({
-    name: NFT_NAME,
-  });
-  fileData.append("pinataMetadata", fileMetadata);
-
-  let pinFileRes = await fetch(PIN_FILE_URL, {
-    method: "POST",
-    maxBodyLength: "Infinity",
-    headers: {
-      "Authorization": `Bearer ${process.env.PINATA_JWT_TOKEN}`,
-    },
-    body: fileData
-  });
-
-  if(pinFileRes.status === 200) {
-    let pinFileResData = await pinFileRes.json();
-    nftImgCID = pinFileResData.IpfsHash;
-
-    let nftMetadata = {
-      name: NFT_NAME,
-      price: NFT_PRICE,
-      img: nftImgCID
+// Define an asynchronous function to pin a file to IPFS
+async function pinToIPFS(url, data, apiType) {
+  try {
+    if(apiType === 'JSON') {
+      pinataHeaders = {
+        ...pinataHeaders,
+        "Content-Type": "application/json" 
+      }
     }
-
-    let jsonMetadata = JSON.stringify(nftMetadata);
-
-    let pinMetadataRes = await fetch(PIN_METADATA_URL, {
+    // Make a POST request to the specified URL with provided headers and form data
+    const response = await fetch(url, {
       method: "POST",
-      headers: {
-        "Content-Type": 'application/json',
-        "Authorization": `Bearer ${process.env.PINATA_JWT_TOKEN}`,
-      },
-      body: jsonMetadata
+      headers: { ...pinataHeaders },
+      body: data,
     });
 
-    let pinMetadataResData = await pinMetadataRes.json();
-    nftMetadataCID = pinMetadataResData.IpfsHash;
-  
-    console.log(pinMetadataResData)
+    // Check if the response is successful; otherwise, throw an error
+    if (response.statusText !== 'OK') {
+      throw new Error(`Failed to pin to IPFS. Status: ${response.status}`);
+    }
+
+    // Parse the response JSON and return the IPFS hash
+    const responseData = await response.json();
+    return responseData.IpfsHash;
+  } catch (error) {
+    // Log and rethrow any errors that occur during the pinning process
+    console.error("Error pinning to IPFS:", error.message);
+    throw error;
   }
+}
 
+// Export an asynchronous function named POST to handle incoming HTTP POST requests
+export async function POST(request) {
+  try {
+    // Parse the incoming form data from the HTTP request
+    const formData = await request.formData();
 
-  return NextResponse.json(nftMetadataCID);
+    // Extract relevant data from the form fields
+    const NFT_NAME = formData.get("name");
+    const NFT_PRICE = formData.get("price");
+    const NFT_FILE = formData.get("file");
+
+    // Create a new FormData object for the NFT file and its metadata
+    const fileData = new FormData();
+    fileData.append("file", NFT_FILE);
+    fileData.append("pinataMetadata", JSON.stringify({ name: NFT_NAME }));
+
+    // Pin the NFT file to IPFS and get the resulting CID
+    const nftImgCID = await pinToIPFS(PINATA_FILE_URL, fileData, 'FILE');
+
+    // Create metadata for the NFT, including the name, price, and image CID
+    const nftMetadata = {
+      name: NFT_NAME,
+      price: NFT_PRICE,
+      img: nftImgCID,
+    };
+
+    // Pin the NFT metadata to IPFS and get the resulting CID
+    const nftMetadataCID = await pinToIPFS(
+      PINATA_METADATA_URL,
+      JSON.stringify(nftMetadata),
+      'JSON'
+    );
+
+    // Log success message and return the metadata CID as JSON response
+    console.log("Successfully pinned metadata:", nftMetadataCID);
+    return NextResponse.json(nftMetadataCID);
+  } catch (error) {
+    // Log and return an error response if an exception occurs during processing
+    console.error("Error processing NFT:", error.message);
+    return NextResponse.error("Internal Server Error", { status: 500 });
+  }
 }
