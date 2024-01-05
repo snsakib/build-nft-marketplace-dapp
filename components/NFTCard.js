b89f27298fbeba4bde18da6cbea782e7a7733ca8
snsakib <hello@snsakib.com>
2024-01-05 08:16:03 +0600

    Add NFTCard component and configure Next.js image remote patterns

---
+++ components/NFTCard.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function NFTCard(data) {
+  let imgURL = 'https://ipfs.io/ipfs/' + data.data.img
+  return (
+    <div className="border-2 border-sky-900 rounded w-10/12">
+      <div>
+        <div className="relative h-[300px]">
+          <Image src={imgURL} layout="fill" alt="Image of the NFT"/>
+        </div>
+        <div className="p-3 text-black">
+          <h1 className="font-bold text-xl">{data.data.name}</h1>
+          <p className="text-base mt-2 font-medium">{data.data.price} ETH</p>
+        </div>
+      </div>
+      <div className="py-3 mx-3">
+        <button
+          className="bg-blue-500 rounded p-2 w-full font-bold"
+        >
+          Buy NFT
+        </button>
+      </div>
+    </div>
+  );
+}
